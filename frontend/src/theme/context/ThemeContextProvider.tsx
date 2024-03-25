641744dd42ab1de9be08b6812f427f7a1cbb7130
julio <julio.sanic.gt.256@gmail.com>
2024-03-25 10:38:56 -0600

    localStorage tema

--- frontend/src/theme/context/ThemeContextProvider.tsx
+++ frontend/src/theme/context/ThemeContextProvider.tsx
@@ -1,6 +1,12 @@
-import React, { useReducer } from "react"
+import React, { useEffect, useReducer } from "react"
 import { themeContext } from "./context-theme"
 import themeReducer from "./themeReducer"
+
+const initialTheme = localStorage.getItem('theme') ? 
+JSON.parse(localStorage.getItem('theme') as string) : {
+  primary: 'color-7',
+  background: 'background-1'
+}
 
 const ThemeContextProvider = (
     {
@@ -9,15 +15,16 @@ const ThemeContextProvider = (
         children: React.ReactNode
     }
 ) => {
-  const [theme, dispatchTheme] = useReducer(themeReducer, {
-    primary: 'color-1',
-    background: 'background-1'
-  })  
+  const [theme, dispatchTheme] = useReducer(themeReducer, initialTheme)  
 
   const setTheme = (theme: Primary | Background) => {
     dispatchTheme({ type: theme })
     console.log(theme)
   }
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme))
+  }, [theme])
 
   return (
     <themeContext.Provider value={{
